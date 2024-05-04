c9a98ca82f320856251646319badb2a204d71b7c
Clarence Lubrin <clarencelubrin@gmail.com>
2024-05-04 18:27:44 +0800

    Add files via upload

---
+++ Scanning RFID.ino
@@ -0,0 +1,270 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <HTTPSRedirect.h>
+
+#include<Wire.h>
+#include<LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// ======================== SETTINGS ========================
+
+// Enter Google Script Deployment ID:
+const char *GScriptId = "AKfycbzMx3apYeijh7VqqkneBIMS2aSS2PctnfIMHnR2rmOfT5e1WUfp2J_lAbP0m82oFnk";
+String gate_number = "Gate1";
+
+// Enter WIFI:
+const char* ssid     = "PLDTHOMEFIBRb8c30";
+const char* password = "PLDTWIFIx9kzh";
+
+// Show WIFI name in the LCD
+const bool showSSID = true;
+// How many reconnection attempts
+const int reconAttemps = 5;
+
+// ==========================================================
+
+String payload_base =  "{\"command\": \"insert_row\", \"sheet_name\": \"Sheet3\", \"values\": ";
+String payload = "";
+
+const char* host        = "script.google.com";
+const int   httpsPort   = 443;
+const char* fingerprint = "";
+
+String url = String("/macros/s/") + GScriptId + "/exec";
+HTTPSRedirect* client = nullptr;
+
+String student_id;
+
+int blocks[] = {4,5,6,8,9};
+
+#define total_blocks  (sizeof(blocks) / sizeof(blocks[0]))
+#define RST_PIN  0  //D3
+#define SS_PIN   2  //D4
+#define BUZZER   4  //D2
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;  
+MFRC522::StatusCode status;
+
+int blockNum = 2;  
+
+byte bufferLen = 18;
+byte readBlockData[18];
+
+void setup() {
+  Serial.begin(9600);        
+  delay(10);
+  Serial.println('\n');
+
+  SPI.begin();
+
+  InitLCD();            // Initialize the LCD
+  ConnectWifi();        // Connect to Wifi
+  ConnectToGoogle();    // Connect to Google
+}
+
+void loop() {
+  static bool flag = false;
+  if (!flag){
+    client = new HTTPSRedirect(httpsPort);
+    client->setInsecure();
+    flag = true;
+    client->setPrintResponseBody(true);
+    client->setContentTypeHeader("application/json");
+  }
+  
+  // If disconnected
+  if (client != nullptr){
+    if (!client->connected()){
+      int retval = client->connect(host, httpsPort);
+      if (retval != 1){
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Disconnected.");
+        lcd.setCursor(0,1); 
+        lcd.print("Retrying...");
+        return; // Exit
+      }
+    }
+  }
+
+  // Scanning for RFID CARD
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Scan your card");
+
+  mfrc522.PCD_Init();
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    delay(1000);
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    delay(1000);
+    return;
+  }
+
+  // Read Data from RFID Card
+  String values = "", data;
+
+  for (byte i = 0; i < total_blocks; i++) {
+    ReadDataFromBlock(blocks[i], readBlockData);
+    if(i == 0){
+      data = String((char*)readBlockData);
+      data.trim();
+      student_id = data;
+      values = "\"" + data + ",";
+    }
+    else{
+      data = String((char*)readBlockData);
+      data.trim();
+      values += data + ",";
+    }
+  }
+  // User values to be uploaded to Google Sheets [ JSON ]
+  values += gate_number + "\"}";
+  payload = payload_base + values;
+
+  //----------------------------------------------------------------
+  lcd.clear();
+  lcd.setCursor(0,0); 
+  lcd.print("Writing Data");
+  lcd.setCursor(0,1); 
+  lcd.print("to Google Sheets...");
+  //----------------------------------------------------------------
+  // Publish data to Google Sheets
+  if(client->POST(url, host, payload)){ 
+    lcd.clear();
+    lcd.setCursor(0,0); 
+    lcd.print("Dangal Greetings ");
+    lcd.setCursor(0,1); 
+    lcd.print("ID:" + student_id);
+
+    delay(2500);
+
+    lcd.clear();
+    lcd.setCursor(0,0); 
+    lcd.print("Login Success.");
+  }
+  else{
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Failed.");
+    lcd.setCursor(0,1); 
+    lcd.print("Try Again");
+  } 
+  delay(1000);
+}
+
+//
+//   Initialiation methods 
+//
+
+/*           LCD             */
+
+void InitLCD(){
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+}
+
+/*      WIFI AND NETWORK      */
+void ConnectWifi(){
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+
+  lcd.setCursor(0,0);
+  lcd.print("Connecting to ");
+  lcd.setCursor(0,1);
+  if (showSSID){
+    lcd.print(ssid);
+  }
+  else{
+    lcd.print("WiFi...");
+  }
+  delay(5000);
+}
+
+void ConnectToGoogle(){
+  client = new HTTPSRedirect(httpsPort);
+  client->setInsecure();
+  client->setPrintResponseBody(true);
+  client->setContentTypeHeader("application/json");
+
+  lcd.clear();
+  lcd.setCursor(0,0); 
+  lcd.print("Connecting to");
+  lcd.setCursor(0,1);
+  lcd.print("Google ");
+  delay(5000);
+
+  // Connect with Google server reconAttemps times
+  bool isConnected = false;
+  for(int i=0; i < reconAttemps; i++){ 
+    int retval = client->connect(host, httpsPort);
+    if (retval == 1){
+      isConnected = true;
+      lcd.clear();
+      lcd.setCursor(0,0); 
+      lcd.print("Connected to");
+      lcd.setCursor(0,1); 
+      lcd.print("Google Sheets");
+      delay(2000);
+      break;
+    }
+    else
+      lcd.setCursor(0,0); 
+      lcd.print("Connection failed. Retrying...");
+  }
+  
+  // If connection is past reconAttemps times.
+  if (!isConnected){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Connection failed");
+    lcd.setCursor(0,1);
+    lcd.print("Cannot be reached.");
+    
+    delay(5000);
+    return;
+  }
+  
+  // Terminate
+  delete client;    
+  client = nullptr; 
+}
+
+//
+//   Reading and Encoding methods 
+//
+
+/*       READING DATA        */
+void ReadDataFromBlock(int blockNum, byte readBlockData[]) 
+{ 
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK){
+     Serial.print("Authentication failed for Read: ");
+     Serial.println(mfrc522.GetStatusCodeName(status));
+     return;
+  }
+   else {
+    Serial.println("Authentication success");
+  }
+  status = mfrc522.MIFARE_Read(blockNum, readBlockData, &bufferLen);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print("Reading failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  else {
+    readBlockData[16] = ' ';
+    readBlockData[17] = ' ';
+  }
+}
+
+
---
+++ Writing Data to RFID.ino
@@ -0,0 +1,174 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+const uint8_t RST_PIN = D3;
+const uint8_t SS_PIN = D4;
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;        
+
+int blockNum = 4;
+
+byte bufferLen = 18;
+byte readBlockData[18];
+
+MFRC522::StatusCode status;
+
+void setup() 
+{
+  Serial.begin(9600);
+  SPI.begin();
+
+  //Initialize the Scanner
+  mfrc522.PCD_Init();
+  Serial.println("Scan the RFID Card write data...");
+}
+
+void loop()
+{
+  // Write the hexadecimal key
+  for (byte i = 0; i < 6; i++){
+    key.keyByte[i] = 0xFF; //FFFFFFFFFFFFh
+  }
+
+  // Check if card is present
+  if (!mfrc522.PICC_IsNewCardPresent()){
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Card Detected
+  Serial.print("\n");
+  Serial.println("**Card Detected**");
+
+  ReadCardData();       // Read Information of the Card
+  WriteCardData();      // Write Information to the Card
+}
+
+//
+//   Read and Write methods 
+//
+
+/*           Read            */
+void ReadCardData(){
+  /* Print UID of the Card */
+  Serial.print(F("Card UID:"));
+  for (byte i = 0; i < mfrc522.uid.size; i++){
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.print("\n");
+
+  /* Print the type of the Card */
+  Serial.print(F("Card type: "));
+  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+  Serial.println(mfrc522.PICC_GetTypeName(piccType));
+}
+
+/*           Write            */
+void WriteCardData(){
+  byte buffer[18];
+  byte len;
+  
+  Serial.setTimeout(20000L); // Wait for user prompt for 20 seconds
+
+  Serial.println(F("---------------------------------------"));
+  Serial.println(F("Enter Student ID, ending with #"));
+  len = Serial.readBytesUntil('#', (char *) buffer, 16);
+
+  //add empty spaces to the remaining bytes of buffer
+  for (byte i = len; i < 16; i++) buffer[i] = ' ';
+  blockNum = 4;
+  WriteDataToBlock(blockNum, buffer);
+  ReadDataFromBlock(blockNum, readBlockData);
+  dumpSerial(blockNum, readBlockData);
+
+  Serial.println(F("---------------------------------------"));
+  Serial.println(F("Enter First Name, ending with #"));
+  len = Serial.readBytesUntil('#', (char *) buffer, 16);
+  for (byte i = len; i < 16; i++) buffer[i] = ' ';
+  blockNum = 5;
+  WriteDataToBlock(blockNum, buffer);
+  ReadDataFromBlock(blockNum, readBlockData);
+  dumpSerial(blockNum, readBlockData);
+
+  Serial.println(F("---------------------------------------"));
+  Serial.println(F("Enter Last Name, ending with #"));
+  len = Serial.readBytesUntil('#', (char *) buffer, 16);
+  for (byte i = len; i < 16; i++) buffer[i] = ' ';
+  blockNum = 6;
+  WriteDataToBlock(blockNum, buffer);
+  ReadDataFromBlock(blockNum, readBlockData);
+  dumpSerial(blockNum, readBlockData);
+
+  Serial.println(F("---------------------------------------"));
+  Serial.println(F("Enter Middle Initial, ending with #"));
+  len = Serial.readBytesUntil('#', (char *) buffer, 16);
+  for (byte i = len; i < 16; i++) buffer[i] = ' ';
+  blockNum = 8;
+  WriteDataToBlock(blockNum, buffer);
+  ReadDataFromBlock(blockNum, readBlockData);
+  dumpSerial(blockNum, readBlockData);
+}
+
+void WriteDataToBlock(int blockNum, byte blockData[]) 
+{
+  /* Authenticating the desired data block for write access using Key A */
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK){
+    Serial.print("Authentication failed for Write: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+ 
+  /* Write data to the block */
+  status = mfrc522.MIFARE_Write(blockNum, blockData, 16);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print("Writing to Block failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+}
+
+void ReadDataFromBlock(int blockNum, byte readBlockData[]) 
+{
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+  /* Authenticating the desired data block for Read access using Key A */
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
+
+  if (status != MFRC522::STATUS_OK){
+   Serial.print("Authentication failed for Read: ");
+   Serial.println(mfrc522.GetStatusCodeName(status));
+   return;
+  }
+
+  /* Reading data from the Block */
+  status = mfrc522.MIFARE_Read(blockNum, readBlockData, &bufferLen);
+  if (status != MFRC522::STATUS_OK){
+    Serial.print("Reading failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+-
+}
+
+void dumpSerial(int blockNum, byte blockData[]) 
+{
+  Serial.print("\n");
+  Serial.print("Data saved on block");
+  Serial.print(blockNum);
+  Serial.print(": ");
+  for (int j=0 ; j<16 ; j++){
+    Serial.write(readBlockData[j]);
+  }
+  Serial.print("\n");
+
+  //Empty readBlockData array
+  for( int i = 0; i < sizeof(readBlockData);  ++i )
+   readBlockData[i] = (char)0; //empty space
+}
